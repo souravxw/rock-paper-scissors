693548792f8551c17823ba9ee1a7193e1629d4c8
souravxw <codersourav245@gmail.com>
2024-11-25 18:50:10 +0530

    complete entire game
    playRound function created and moved to playGame function , then for five round playRound function called five times.
    finally showed who is the winner depending on score.

--- script.js
+++ script.js
@@ -34,7 +34,88 @@ function getHumanChoice() {
 }
 
 
-//score count
-let humanScore = 0,
-    computerScore = 0;
+// //score count
+// let humanScore = 0;
+// let computerScore = 0;
 
+
+// const humanSelection = getHumanChoice();
+// const computerSelection = getComputerChoice();
+
+
+    
+
+
+    
+
+
+    
+
+
+function playGame() {
+    //score count
+let humanScore = 0;
+let computerScore = 0;
+
+//playround function
+function playRound(humanChoice, computerChoice) {
+    
+
+
+    if(humanChoice === computerChoice ) {
+     alert("tie!");
+    } else {
+     if(humanChoice === 'paper' && computerChoice === 'rock' || 
+         humanChoice === "scissors"  && computerChoice === 'paper' ||
+         humanChoice === "rock" && computerChoice === "scissors"
+     ) { 
+         console.log("You win!");
+         humanScore = humanScore + 1;
+         
+     } else {
+         console.log("Computer win!");
+         computerScore = computerScore + 1;
+         
+     }
+ 
+    }
+    
+ }
+//play 5 times
+const humanSelection1 = getHumanChoice();
+const computerSelection1 = getComputerChoice();
+playRound(humanSelection1,computerSelection1);
+
+const humanSelection2 = getHumanChoice();
+const computerSelection2 = getComputerChoice();
+playRound(humanSelection2,computerSelection2);
+
+const humanSelection3 = getHumanChoice();
+const computerSelection3 = getComputerChoice();
+playRound(humanSelection3,computerSelection3);
+const humanSelection4 = getHumanChoice();
+const computerSelection4 = getComputerChoice();
+playRound(humanSelection4,computerSelection4);
+const humanSelection5 = getHumanChoice();
+const computerSelection5 = getComputerChoice();
+playRound(humanSelection5,computerSelection5);
+  
+
+
+//showing final result after playing 5 rounds
+
+if(humanScore > computerScore) {
+    console.log("You are the Winner!");
+    alert(`You are the WINNER! Score = ${humanScore}/5`);
+} else if ( humanScore < computerScore) {
+    console.log("Computer is the Winner!");
+    alert(`Computer is the WINNER! Score = ${computerScore}/5`);
+} else {
+    console.log("TIE! play again");
+    alert(`TIE! ${humanScore} vs ${computerScore}`);
+}
+
+}
+    
+
+playGame();
